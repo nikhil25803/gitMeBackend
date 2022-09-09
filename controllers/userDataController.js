7a87adb771b841d8613b2e7e0dc3b4d461a1f909
Nikhil Raj <nikhil25803@gmail.com>
2022-09-10 00:24:41 +0530

    ENV Setup

--- controllers/userDataController.js
+++ controllers/userDataController.js
@@ -1,15 +1,19 @@
+require('dotenv').config()
 const axios = require('axios')
 const url = require('url')
 
 exports.profileData = (req, res, next) => {
     const urlData = url.parse(req.url, true).query
     const userName = urlData['profile']
-    const headers = {
-        'Content-Type': 'application/json',
-        'Authorization': 'JWT fefege...'
+    const config = {
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': process.env.GITHUB_ACCESS_KEY
+        }
     }
+
     axios
-        .get(`https://api.github.com/users/${userName}`)
+        .get(`https://api.github.com/users/${userName}`, config)
         .then(result => {
             const data = result
             res.send(data['data'])
